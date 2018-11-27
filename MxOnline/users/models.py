7148484c7e893ef5acd496bbeac57fd02c503a92
Ihtml <2483690020@qq.com>
2018-11-27 23:42:15 +0800

    add  EmailVerifyRecord and Banner models

--- MxOnline/users/models.py
+++ MxOnline/users/models.py
@@ -1,10 +1,16 @@
 # _*_ encoding:utf-8 _*_
-from django.db import models
 
-# Create your models here.
+# 第一块用于引用python自带的包
+from datetime import datetime
+
+# 第二块取第三方，块之间用一行分割
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# Create your models here.
 
+
+# 用户信息
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
     birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
@@ -19,3 +25,31 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+# 邮箱验证
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
+    send_type = models.CharField(verbose_name=u"验证码类型", choices=(("register",u"注册"),("forget",u"找回密码"), ("update_email",u"修改邮箱")), max_length=30)
+    send_time = models.DateTimeField(verbose_name=u"发送时间", default=datetime.now) #不加括号是class生成的时间，加括号是编译的时间
+
+    class Meta:
+        verbose_name = u"邮箱验证码"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}({1})'.format(self.code, self.email)
+
+
+# 轮播图
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u"标题")
+    image = models.ImageField(upload_to="banner/%Y/%m", verbose_name=u"轮播图", max_length=100)
+    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    index = models.IntegerField(default=100, verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"轮播图"
+        verbose_name_plural = verbose_name
