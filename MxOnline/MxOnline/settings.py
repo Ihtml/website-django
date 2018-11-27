4a8417d42e297307f97b195ce664f6f2c7850548
Ihtml <2483690020@qq.com>
2018-11-28 00:23:46 +0800

    add courses app and models

--- MxOnline/MxOnline/settings.py
+++ MxOnline/MxOnline/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'courses',
 ]
 # 覆盖原有的user
 AUTH_USER_MODEL = "users.UserProfile"
