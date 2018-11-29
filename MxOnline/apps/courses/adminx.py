5e179607c4877940cd0197460b6915c732217ee7
Ihtml <2483690020@qq.com>
2018-11-29 23:57:11 +0800

    add course xadmin

---
+++ MxOnline/apps/courses/adminx.py
@@ -0,0 +1,33 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'ife'
+__date__ = '2018-11-29 23:35'
+
+import xadmin
+
+from .models import Course, Lesson, CourseResource
+
+
+class CourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+    ordering = ['-click_nums']
+    readonly_fields = ['click_nums']
+    exclude = ['fav_nums']
+
+
+class LessonAdmin(object):
+    list_display = ['course', 'name', 'add_time']
+    search_fields = ['course', 'name']
+    list_filter = ['course__name', 'name', 'add_time']
+
+
+class CourseResourceAdmin(object):
+    list_display = ['course', 'name', 'download', 'add_time']
+    search_fields = ['course', 'name', 'download']
+    list_filter = ['course', 'name', 'download', 'add_time']
+
+
+xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson, LessonAdmin)
+xadmin.site.register(CourseResource, CourseResourceAdmin)
