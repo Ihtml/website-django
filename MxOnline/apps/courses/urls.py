b1ec5dd2e2c3f5646e0a97aa65461631a6bcd27f
Ihtml <2483690020@qq.com>
2018-12-04 22:27:43 +0800

    add courses urls.py

--- MxOnline/apps/courses/urls.py
+++ MxOnline/apps/courses/urls.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 __author__ = 'ife'
-__date__ = '2018-12-02 21:18'
+__date__ = '2018-12-04 22:12'
 
 from django.urls import path
 from django.views.generic import TemplateView
@@ -8,7 +8,6 @@ from . import views
 
 
 # 命名空间
-app_name = 'users'
+app_name = 'courses'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name="index"),
 ]
--- MxOnline/apps/users/views.py
+++ MxOnline/apps/users/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+from django.contrib.auth import authenticate, login
 
 
-def login(request):
-    if request.method == 'POST':
-        pass
-    elif request.method == "GET":
+class LoginView(View):
+    def get(self, request):
         return render(request, "login.html", {})
+
+    def post(self, request):
+        user_name = request.POST.get("username", "")
+        pass_word = request.POST.get("password", "")
+        user = authenticate(user_name, pass_word)
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
