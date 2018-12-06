dff1d2fcb6df317e1434e477a2795d42cdfb5940
Ihtml <2483690020@qq.com>
2018-12-06 23:36:55 +0800

    add import

--- MxOnline/MxOnline/urls.py
+++ MxOnline/MxOnline/urls.py
@@ -13,6 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
