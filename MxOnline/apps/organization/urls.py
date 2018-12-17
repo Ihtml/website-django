d94725dd2d00f51b785a3935b2a3216965fd6ecf
Ihtml <2483690020@qq.com>
2018-12-17 23:29:24 +0800

    add utils

---
+++ MxOnline/apps/organization/urls.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'ife'
+__date__ = '2018-12-17 22:58'
+
+from django.urls import path, include
+
+# from .views import OrgCourseView
+#
+# urlpatterns = [
+#     path('course/<int:org_id>', OrgCourseView.as_view(), name="org_course"),
+# ]
